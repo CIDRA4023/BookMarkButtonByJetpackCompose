9e85fe90c6447e143ec1a5b8d70e0f7891910c3b
CIDRA <cidra3204@gmail.com>
2022-01-24 22:42:44 +0900

    add: Screen transition buttons

--- app/src/main/java/com/example/bookmarkbuttonbyjetpackcompose/DetailScreen.kt
+++ app/src/main/java/com/example/bookmarkbuttonbyjetpackcompose/DetailScreen.kt
@@ -1,9 +1,28 @@
 package com.example.bookmarkbuttonbyjetpackcompose
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 
 @Composable
 fun DetailScreen(navController: NavController) {
+    Scaffold(content = {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize() ) {
 
+            Button(onClick = { navController.popBackStack() }) {
+                Text(text = "Main")
+            }
+            Text(text = "DetailScreen")
+        }
+    })
 }
--- app/src/main/java/com/example/bookmarkbuttonbyjetpackcompose/MainScreen.kt
+++ app/src/main/java/com/example/bookmarkbuttonbyjetpackcompose/MainScreen.kt
@@ -1,9 +1,28 @@
 package com.example.bookmarkbuttonbyjetpackcompose
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 
 @Composable
 fun MainScreen(navController: NavController) {
+    Scaffold(content = {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize() ) {
 
+            Button(onClick = { navController.navigate("detail") }) {
+                Text(text = "Detail")
+            }
+            Text(text = "MainScreen")
+        }
+    })
 }
