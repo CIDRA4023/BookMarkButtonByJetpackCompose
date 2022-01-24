f11b93c9f695f692a26d5e51ce00f4544d783977
CIDRA <cidra3204@gmail.com>
2022-01-24 22:29:21 +0900

    add: ApplicationClass

---
+++ app/src/main/java/com/example/bookmarkbuttonbyjetpackcompose/BookMarkApplication.kt
@@ -0,0 +1,8 @@
+package com.example.bookmarkbuttonbyjetpackcompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BookMarkApplication: Application() {
+}
